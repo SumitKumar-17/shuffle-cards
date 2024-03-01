7bcf7b86107c9f3ddbaad84412865b3a9b38fd9f
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-01 08:51:44 +0530

    making a shuffle card page

---
+++ src/components/generateSquares.jsx
@@ -0,0 +1,19 @@
+import squareData from "../data/data";
+import shuffle from "./Shuffle";
+import { motion } from "framer-motion";
+const generateSquares = () => {
+  return shuffle(squareData).map((sq) => (
+    <motion.div
+      key={sq.id}
+      layout
+      transition={{ duration: 1.5, type: "spring" }}
+      className="w-full h-full"
+      style={{
+        backgroundImage: `url(${sq.src})`,
+        backgroundSize: "cover",
+      }}
+    ></motion.div>
+  ));
+};
+
+export default generateSquares;

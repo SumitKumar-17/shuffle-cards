1b0c1827fd49b1a41ed1cc3dec4f31c217e3470d
Sumit <sumitkumar@kgpian.iitkgp.ac.in>
2024-03-01 08:58:07 +0530

    adding a shuffling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import ShuffleHero from './components/ShuffleHero';
 
 function App() {
   return (
-    <div className="App flex items-center ">
-      <h1>Shuffle Cards</h1>
-      <ShuffleHero />
+    <div className="App flex flex-col items-center ">
+      <h1 className="p-4 text-4xl">Shuffle Cards</h1>
+      <div className="border border-t-4">
+        <ShuffleHero />
+      </div>
     </div>
   );
 }
